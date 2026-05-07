dbfe2863aaa94ffa68c69ab9381ca3bc4c4d58dc
AzGamer <51761728+AzGamer@users.noreply.github.com>
2026-05-07 14:41:01 +0200

    deploy : github pages via action

--- epsilon-chimie/vite.config.js
+++ epsilon-chimie/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' 
+import js from '@eslint/js'
 
 export default defineConfig({
   base: '/epsilon-chimie-official/',
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),js()],
 })
