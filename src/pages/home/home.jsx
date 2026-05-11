c3095295a90ed1afb6d1aa8123d890b0f7a1d6c2
AzGamer <51761728+AzGamer@users.noreply.github.com>
2026-05-11 14:34:53 +0200

    carousel

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,24 @@
 import './home.css'
+import { MedalIcon } from '../../assets/icons/medal_icon'
+import { TimeIcon } from '../../assets/icons/time_icon'
+import { LocIcon } from '../../assets/icons/localisation_icon'
+import { HealthIcon } from '../../assets/icons/health_icon'
+
+
+
 
 function Home() {
+    const items = [
+        { icon: <MedalIcon color='var(--color-content-reversed-primary)'/>, label: 'ICH-Q7 Quality assurance' },
+        { icon: <HealthIcon color='var(--color-content-reversed-primary)'/>, label: 'Every lot above 98% purity' },
+        { icon: <TimeIcon color='var(--color-content-reversed-primary)'/>, label: 'Acknowledgment mostly within 24h' },
+        { icon: <LocIcon color='var(--color-content-reversed-primary)'/>, label: '100% of batches synthesised in Brest' },
+    ]
+    const repeated = [...items, ...items, ...items]
+
     return (
     <>
+        {/* top */}
         <section>
             <article>
                 <div>
@@ -18,7 +34,7 @@ function Home() {
             </article>
 
             <article>
-                
+
                 <img src="" alt="" />
                 <ul>
                     <li><span>1 000+</span>REFERENCES</li>
@@ -26,7 +42,36 @@ function Home() {
                     <li><span>ICH-Q7</span>QUALITY ASSURANCE</li>
                 </ul>
             </article>
-        </section>        
+        </section>
+
+        {/* carousel */}
+        <div className="carousel">
+            <div className="carousel__track">
+                <ul>
+                    {repeated.map((item, i) => (
+                        <li key={i}>{item.icon}{item.label}</li>
+                    ))}
+                </ul>
+                <ul aria-hidden="true">
+                    {repeated.map((item, i) => (
+                        <li key={i}>{item.icon}{item.label}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+
+        <section>
+            <article>
+                <legend><div></div>COMPOUND FINDER</legend>
+                <h2>Search across 1 000+ references</h2>
+                <p>Search by IUPAC name, CAS number, MFCD, synonym or molecular formula. Can't finde what you need ? We synthesis on demand.</p>
+            </article>
+
+            <article>
+                <div></div>
+                <div></div>
+            </article>
+        </section>
     </>
     )
 }
