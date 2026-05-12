448f833e5d392150d6417e7fb7e4cf06a8a7258e
AzGamer <51761728+AzGamer@users.noreply.github.com>
2026-05-12 15:57:24 +0200

    capabilities

---
+++ epsilon-chimie/src/assets/icons/right_arrow_icon.jsx
@@ -0,0 +1,8 @@
+export const RightArrowIcon = ({ size = 24}) => {
+    return (
+        <svg width={size} height={size} viewBox={`0 0 24 24`} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.4302 5.92969L20.5002 11.9997L14.4302 18.0697" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M3.5 12H20.33" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+    );
+};
