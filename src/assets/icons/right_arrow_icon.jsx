08b4f5c4d66cb2babd9ab731a33ab03f12e8cd4d
AzGamer <51761728+AzGamer@users.noreply.github.com>
2026-05-13 13:16:22 +0200

    fix icon

--- src/assets/icons/right_arrow_icon.jsx
+++ src/assets/icons/right_arrow_icon.jsx
@@ -1,8 +1,14 @@
-export const RightArrowIcon = ({ size = 24}) => {
+import PropTypes from 'prop-types';
+
+export const RightArrowIcon = ({ size = 24 }) => {
     return (
-        <svg width={size} height={size} viewBox={`0 0 24 24`} fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M14.4302 5.92969L20.5002 11.9997L14.4302 18.0697" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M3.5 12H20.33" stroke="currentColor" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
+        <svg width={size} height={size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14.4302 5.92969L20.5002 11.9997L14.4302 18.0697" stroke="currentColor" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M3.5 12H20.33" stroke="currentColor" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     );
+};
+
+RightArrowIcon.propTypes = {
+    size: PropTypes.number,
 };
