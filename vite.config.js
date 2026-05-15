f67f2b1309e13eef8832f74f085a18d88c6b66eb
AzGamer <51761728+AzGamer@users.noreply.github.com>
2026-05-15 12:34:38 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite' 
-import js from '@eslint/js'
-
 
 export default defineConfig({
   base: '/epsilon-chimie-official/',
-  plugins: [react(),tailwindcss(),js()],
+  plugins: [react(),tailwindcss()],
 })
