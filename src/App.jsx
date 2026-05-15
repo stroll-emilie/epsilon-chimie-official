b0e309e086596e002d5ac8010f4f9f4918fdc292
Gabin CHEVALIER <gabinche29@gmail.com>
2026-05-15 10:33:25 +0200

    start

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import './styles/App.css'
 import Header from './components/Header/header.jsx'
 import Footer from './components/Footer/footer.jsx'
 import Home from './pages/home/home.jsx'
+import About from './pages/about/about.jsx'
 
 function App() {
   return (
     <main>
       <Header />
-      <Home />
+      <About />
       <Footer />
     </main>
   )
---
+++ src/pages/about/about.jsx
@@ -0,0 +1,33 @@
+import './about.css'
+
+function About() {
+    return (
+    <>
+        <section id="about-laboratory">
+            <div>
+                <article>
+                    <div className='path'>HOME / <span>ABOUT</span></div>
+                    <legend><div className='losange'></div>ABOUT THE LABORATORY</legend>
+                    <h1>A french laboratory serving worldwide industry since 1995.</h1>
+                    <p>Located in Brest, France, Epsilon Chimie formulates, analyses and produces speciality phosphorus chemicals for research and industry. Our founders trained in organometallic chemistry lab of the University of Brest; our product range still reflects that heritage.</p>
+                </article>
+                <img src="" alt="" />
+            </div>
+            <article>
+                <ul>
+                    <li><span>1 000+</span>REFERENCES</li>
+                    <li><span>30+ YRS</span>OF EXPERIENCE</li>
+                    <li><span>ICH-Q7</span>QUALITY ASSURANCE</li>
+                </ul>
+            </article>
+        </section>
+
+        <section id="quality">
+            <article></article>
+            <article></article>
+        </section>
+    </>
+    )
+}
+
+export default About
