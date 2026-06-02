5ef02a966e825191f881a7c2d94717dccd66bd1f
AzGamer <51761728+AzGamer@users.noreply.github.com>
2026-06-02 14:03:10 +0200

    error 404

--- src/pages/error/error404.jsx
+++ src/pages/error/error404.jsx
@@ -1,12 +1,28 @@
+import { CircleArrowIcon } from '../../assets/icons/circle_arrow_icon'
+import './error404.css'
 
+import { Link } from "react-router-dom"
 
 function Error404() {
         
     return (
     <>
-        <section>
-            <h1>Page introuvable</h1> <br />
-
+        
+        <section id="error404">
+            <article>
+                <div className='path'><Link to="/">HOME</Link> / <span>ERROR 404</span></div>
+            </article>
+            <article>
+                <div>
+                    <h1>404</h1>
+                    <h2>This pages could not be found.</h2>
+                </div>
+                <p>The page you were looking for may have moved, been renames, or never existed. <br /> Let's get you back to the chemistry.</p>
+                <nav>
+                    <Link to="/"><CircleArrowIcon/> Back Home</Link>
+                    <Link to="/catalogue">Browse the catalogue</Link>
+                </nav>
+            </article>
         </section>
     </>
     )
