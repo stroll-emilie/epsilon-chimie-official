ed0d1570a5cdd9ae11288a78ff94960d0b45ddd8
AzGamer <51761728+AzGamer@users.noreply.github.com>
2026-06-09 15:51:11 +0200

    nouvelle config temporaire

--- src/pages/request/request.jsx
+++ src/pages/request/request.jsx
@@ -12,7 +12,7 @@ import { useEffect, useMemo, useState } from 'react'
 import { getProductById, parseNom, getDefaultPurity} from '../../services/dataService.js'
 import { useProducts } from '../../context/AppContext';
 import { Link, useParams, useLocation, useNavigate } from 'react-router-dom'
-import { EMAILJS_CONFIG } from '../../config/emailjs.js'
+import { EMAILJS_CONFIG } from '../../config/tempo.js'
 
 function Request() {
     const navigate = useNavigate();
