b8296ad33314f8925b0074aa8d98c31b753f0222
AzGamer <51761728+AzGamer@users.noreply.github.com>
2026-06-16 13:41:01 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 
 export default defineConfig({
+  // FIXME: mettre '/' pour l'envoie en prod
   base: '/epsilon-chimie-official/',
   plugins: [react(),tailwindcss()],
 })
